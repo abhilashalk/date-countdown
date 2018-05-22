3d3e706a37351494fce0e6ee25614fc116618314
Stanley Louis <stanlouis@msn.com>
2018-05-22 11:42:57 -0400

    Add js for gui functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,42 @@
-var a = moment()
-var b = moment().add(40, 'days');
-console.log(b.format('YYYY MM DD'));
+$(document).ready(function () {
+    // Init Side nav
+    $('.button-collapse').sideNav();
 
+    // Init Slider
+    $('.slider').slider({
+        indicators: false,
+        height: 600,
+        transition: 500,
+        interval: 6000
+    });
+
+    // Date Picker
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+
+    // Time Picker
+    $('.timepicker').pickatime({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        canceltext: 'Cancel', // Text for cancel-button
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function () {} //Function for after opening timepicker
+    });
+
+    // INIT SELECT LIST
+    $('select').material_select();
+
+    // Init Scrollspy
+    $('.scrollspy').scrollSpy();
+
+});
