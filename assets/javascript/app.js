a8aac854bf3fd66dd3ff557163bde7ed9e48391e
abhilashalk <abhilulk@gmail.com>
2018-05-23 11:31:53 -0400

    code date and time functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    /* Materialize functionality code  */
     // Init Side nav
     $('.button-collapse').sideNav();
 
@@ -11,9 +12,14 @@ $(document).ready(function () {
     });
 
     // Date Picker
+    var dateNow = new Date();
     $('.datepicker').pickadate({
+        disable: [{
+            from: [2009, 5, 1],
+            to: [dateNow]
+        }],
         selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        selectYears: 25, // Creates a dropdown of 15 years to control year,
         today: 'Today',
         clear: 'Clear',
         close: 'Ok',
@@ -38,5 +44,55 @@ $(document).ready(function () {
 
     // Init Scrollspy
     $('.scrollspy').scrollSpy();
+
+    /* end of materialize code */
+
+    /* get input from form-input */
+    $('#form-input').on('submit', function (e) {
+        e.preventDefault();
+
+
+        //Getting the date
+        var countDownDate = $('.datepicker').val()
+
+        //Getting the time input
+        var countDownTime = $('#time').val()
+        var deadLine = countDownDate + " " + countDownTime;
+        console.log('deadLine', deadLine);
+        console.log(countDownTime);
+
+        var deadLineMilliseconds = new Date(deadLine).getTime();
+        console.log('deadLineMilliseconds', deadLineMilliseconds);
+
+        // Update the count down every 1 second
+        var x = setInterval(function () {
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+
+
+
+            var distance = deadLineMilliseconds - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+            // Display the result in the element with id="demo" Display the result in the
+            // element with id="#countdown"
+            var display = $(".card #countdown");
+            display.html('<h3>' + days + "d " + hours + "h " + minutes + "m " + seconds + "s </h3>")
+            if (distance < 0) {
+                clearInterval(x);
+                document
+                    .getElementById("#countdown")
+                    .innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    });
 
 });
