7a5707b0a26d50d50b1248259e0a1ea0468de070
Stanley Louis <stanlouis@msn.com>
2018-05-24 19:28:32 -0400

    Add autocomplete to event input.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,47 +2,55 @@ $(document)
   .ready(function () {
     var clock;
 
-    function displayClock($display, years, months, days, hours, minutes, seconds, duration, clock) {
-      // Display the result in the element with id="demo" Display the result in the
-      // element with id="#countdown" if (years > 0) {   $('#years').html(('0' +
-      // years).slice(-2) + '<span>Year(s)</span>'); } else {   $('#years').hide(); }
-      $('#years').html(('0' + years).slice(-2) + '<span>Year(s)</span>');
-      $('#months').html(('0' + months).slice(-2) + '<span>Months</span>');
-      $('#days').html(('0' + days).slice(-2) + '<span>Days</span>');
-      $('#hours').html(('0' + hours).slice(-2) + '<span>Hours</span>');
-      $('#minutes').html(('0' + minutes).slice(-2) + '<span>Minutes</span>');
-      $('#seconds').html(('0' + seconds).slice(-2) + '<span>Seconds</span>');
-      if (duration <= 0) {
+    function displayClock(t, duration) {
+      $('#years').html(('0' + t.years).slice(-2) + '<span>YEARS</span>');
+      $('#months').html(('0' + t.months).slice(-2) + '<span>MONTHS</span>');
+      $('#days').html(('0' + t.days).slice(-2) + '<span>DAYS</span>');
+      $('#hours').html(('0' + t.hours).slice(-2) + '<span>HOURS</span>');
+      $('#minutes').html(('0' + t.minutes).slice(-2) + '<span>MINUTES</span>');
+      $('#seconds').html(('0' + t.seconds).slice(-2) + '<span>SECONDS</span>');
+
+      if (duration < 0) {
         clearInterval(clock);
         $('#countdown').html('<h4>Countdown Expired</h4>');
+        $('.message').hide();
+        $('#future-date').hide();
+        setTimeout(function () {
+          window
+            .location
+            .reload();
+        }, 3000);
+
       }
     }
 
-    function initTimer(deadLineMilliseconds, $display) {
-      // Update the count down every 1 second
+    function initTimer(deadLineMilliseconds) {
       clock = setInterval(function () {
-        var now = new Date().getTime();
         var x = new moment(deadLineMilliseconds);
-        var y = new moment(now);
-
-        // https://momentjs.com/docs/#/durations/diffing/
+        var y = new moment(new Date().getTime());
         var duration = moment.duration(x.diff(y));
 
         // Time calculations for years days, hours, minutes and seconds
-        var years = duration.get('years');
+        var t = {
+          'years': duration.get('years'),
 
-        var months = duration.get('months');
+          'months': duration.get('months'),
 
-        var days = duration.get('days');
+          'days': duration.get('days'),
 
-        var hours = duration.get('hours');
+          'hours': duration.get('hours'),
 
-        var minutes = duration.get('minutes');
+          'minutes': duration.get('minutes'),
 
-        var seconds = duration.get('seconds');
+          'seconds': duration.get('seconds')
+        }
 
-        displayClock($display, years, months, days, hours, minutes, seconds, duration, clock);
+        displayClock(t, duration);
       }, 1000);
+    }
+
+    function reset() {
+      $('#form-input')[0].reset()
     }
 
     /* get input from form-input */
@@ -60,25 +68,19 @@ $(document)
           $event.text($title);
         }
 
-        // setting the display var
-        var $display = $('#countdown');
-
         //Getting the date
         var countDownDate = $('.datepicker').val();
-        console.log(countDownDate);
-        // Verify date input Getting the time input
+        // Getting the time input
         var countDownTime = $('#time').val();
+
         var deadLine = countDownDate + ' ' + countDownTime;
-        console.log(deadLine);
 
         var deadLineMilliseconds = new Date(deadLine).getTime();
 
-        initTimer(deadLineMilliseconds, $display);
+        initTimer(deadLineMilliseconds);
+
+        $('#future-date').text(`Time until ${countDownDate}`)
       });
 
-    $('.reset').on('click', function () {
-      window
-        .location
-        .reload();
-    });
+    $('.reset').on('click', reset);
   });
